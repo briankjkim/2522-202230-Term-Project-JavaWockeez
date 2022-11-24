14234c35d263dc0433fbe4ef013f757c675d27a4
WallaceTrinh <WTrinh1@my.bcit.ca>
2022-11-24 12:03:47 -0800

    Uploaded new image files, created a BoundaryTest.java file.

--- src/main/java/ca/bcit/comp2522/termproject/javawockeez/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/javawockeez/Player.java
@@ -15,18 +15,16 @@ public class Player {
     public static final int MOVEMENT_SIZE = 20;
     public String name;
     public ImageView viewCharacter;
-    private double x;
-    private double y;
-
-    private double velX = 0;
-    private double velY = 0;
+//    private double x;
+//    private double y;
+//
+//    private double velX = 0;
+//    private double velY = 0;
 
     private Player player;
 
-    public Player(String name, double x, double y) {
+    public Player(String name) {
         this.name = name;
-        this.x = x;
-        this.y = y;
         Image person = new Image("PersonMove2.gif", true);
         viewCharacter = new ImageView(person);
 
@@ -35,75 +33,22 @@ public class Player {
         viewCharacter.setY(personStartCoordinate);
     }
 
-    public void tick() {
-        x += velX;
-        y += velY;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public void setVelX(double velX) {
-        this.velX = velX;
-    }
-
-    public void setVelY(double velY) {
-        this.velY = velY;
-    }
-
     //    /**
 //     * Modifies the position of the image view when an arrow key is pressed.
 //     *
 //     * @param event invoked this method
 //     */
     public void processKeyPress(final KeyEvent event) {
+
         switch (event.getCode()) {
-            if (event.getCode() == KeyCode.UP) {
-                player.setVelY(MOVEMENT_SIZE);
-            }
-            if (event.getCode() == KeyCode.DOWN) {
-                player.setVelY(MOVEMENT_SIZE);
-            }
-            if (event.getCode() == KeyCode.RIGHT) {
-                player.setVelX(MOVEMENT_SIZE);
-            } else if (event.getCode() == KeyCode.LEFT) {
-                player.setVelX(MOVEMENT_SIZE);
-            }
+            case UP -> viewCharacter.setY(viewCharacter.getY() - MOVEMENT_SIZE);
+            case DOWN -> viewCharacter.setY(viewCharacter.getY() + MOVEMENT_SIZE);
+            case RIGHT -> viewCharacter.setX(viewCharacter.getX() + MOVEMENT_SIZE);
+            case LEFT -> viewCharacter.setX(viewCharacter.getX() - MOVEMENT_SIZE);
+            default -> {
+            } // Does nothing if it's not an arrow key
         }
-//        switch (event.getCode()) {
-//            case UP -> viewCharacter.setY(viewCharacter.getY() - MOVEMENT_SIZE);
-//            case DOWN -> viewCharacter.setY(viewCharacter.getY() + MOVEMENT_SIZE);
-//            case RIGHT -> viewCharacter.setX(viewCharacter.getX() + MOVEMENT_SIZE);
-//            case LEFT -> viewCharacter.setX(viewCharacter.getX() - MOVEMENT_SIZE);
-//            default -> {
-//            } // Does nothing if it's not an arrow key
-    }
-
-    public void processKeyRelease(final KeyEvent event) {
-        if (event.getCode() == KeyCode.UP) {
-            player.setVelY(0);
-        }
-        if (event.getCode() == KeyCode.DOWN) {
-            player.setVelY(0);
-        }
-        if (event.getCode() == KeyCode.RIGHT) {
-            player.setVelX(0);
-        } else if (event.getCode() == KeyCode.LEFT) {
-            player.setVelX(0);
-        }
-
     }
 }
+
+
--- src/main/java/ca/bcit/comp2522/termproject/javawockeez/WallaceGameTest.java
+++ src/main/java/ca/bcit/comp2522/termproject/javawockeez/WallaceGameTest.java
@@ -1,61 +1,61 @@
-package ca.bcit.comp2522.termproject.javawockeez;
-
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
-
-
-/**
- * testing Controls.
- */
-public class WallaceGameTest extends Application {
-
-    private Player player;
-    private ImageView viewBackground;
-    private ImageView viewPersonTwo;
-
-    /**
-     * Displays an image that can be moved using the arrow keys.
-     *
-     * @param primaryStage a Stage
-     */
-    public void start(final Stage primaryStage) {
-
-        Image person_two = new Image("boomboom2.gif", true);
-        Image background = new Image("backgroundtest.jpeg", true);
-
-        viewBackground = new ImageView(background);
-        viewPersonTwo = new ImageView(person_two);
-
-        Player player = new Player("guy", 1,1);
-        Group root = new Group(viewBackground, player.viewCharacter, viewPersonTwo);
-
-        final int appWidth = 500;
-        final int appHeight = 800;
-        Scene scene = new Scene(root, appWidth, appHeight);
-
-        // Register the key listener here
-        scene.setOnKeyPressed(player::processKeyPress);
-        scene.setOnKeyPressed(player::processKeyRelease);
-
-        primaryStage.setTitle("Escape Subject 2522");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    public static void controls(String[] args) {
-
-    }
-
-    /**
-     * Launches the JavaFX application.
-     *
-     * @param args command line arguments
-     */
-    public static void main(final String[] args) {
-        launch(args);
-    }
-}
+//package ca.bcit.comp2522.termproject.javawockeez;
+//
+//import javafx.application.Application;
+//import javafx.scene.Group;
+//import javafx.scene.Scene;
+//import javafx.scene.image.Image;
+//import javafx.scene.image.ImageView;
+//import javafx.stage.Stage;
+//
+//
+///**
+// * testing Controls.
+// */
+//public class WallaceGameTest extends Application {
+//
+//    private Player player;
+//    private ImageView viewBackground;
+//    private ImageView viewPersonTwo;
+//
+//    /**
+//     * Displays an image that can be moved using the arrow keys.
+//     *
+//     * @param primaryStage a Stage
+//     */
+//    public void start(final Stage primaryStage) {
+//
+//        Image person_two = new Image("ExplosionSpriteFPS7.gif", true);
+//        Image background = new Image("BackgroundTemplate.png", true);
+//
+//        viewBackground = new ImageView(background);
+//        viewPersonTwo = new ImageView(person_two);
+//
+//        final int explosionStartCoordinateX = 170;
+//        final int explosionStartCoordinateY = 291;
+//        viewPersonTwo.setX(explosionStartCoordinateX);
+//        viewPersonTwo.setY(explosionStartCoordinateY);
+//        Player player = new Player("guy");
+//        Group root = new Group(viewBackground, player.viewCharacter, viewPersonTwo);
+//
+//        final int appWidth = 510;
+//        final int appHeight = 700;
+//        Scene scene = new Scene(root, appWidth, appHeight);
+//
+//        // Register the key listener here
+//        scene.setOnKeyPressed(player::processKeyPress);
+////        scene.setOnKeyPressed(player::processKeyRelease);
+//
+//        primaryStage.setTitle("Escape Subject 2522");
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+//    }
+//
+//    /**
+//     * Launches the JavaFX application.
+//     *
+//     * @param args command line arguments
+//     */
+//    public static void main(final String[] args) {
+//        launch(args);
+//    }
+//}
