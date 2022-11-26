70c9df525a4a3be1ff3b34097c6ad3b728ffb037
WallaceTrinh <WTrinh1@my.bcit.ca>
2022-11-26 14:50:20 -0800

    Added a boundaries for the Player.java file for player restricted movement.

--- src/main/java/ca/bcit/comp2522/termproject/javawockeez/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/javawockeez/Player.java
@@ -2,35 +2,58 @@ package ca.bcit.comp2522.termproject.javawockeez;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.Scene;
-import javafx.scene.shape.MoveTo;
+
+import static ca.bcit.comp2522.termproject.javawockeez.WallaceGameTest.appHeight;
+import static ca.bcit.comp2522.termproject.javawockeez.WallaceGameTest.appWidth;
 
 public class Player {
-
+    public static final double STARTING_X = 240;
+    public static final double STARTING_Y = 600;
     /**
      * The movement size in pixels when the player presses a key.
      */
     public static final int MOVEMENT_SIZE = 20;
+    public static final String IMAGE_CHARACTER = "PersonMove2.gif";
     public String name;
-    public ImageView viewCharacter;
-//    private double x;
-//    private double y;
-//
-//    private double velX = 0;
-//    private double velY = 0;
+    public static ImageView viewCharacter;
 
     private Player player;
+    private static Player singletonPlayer;
 
     public Player(String name) {
         this.name = name;
-        Image person = new Image("PersonMove2.gif", true);
-        viewCharacter = new ImageView(person);
+        this.viewCharacter = new ImageView (new Image("PersonMove2.gif", true));
+//        viewCharacter = new ImageView(person);
+        viewCharacter.setX(STARTING_X);
+        viewCharacter.setY(STARTING_Y);
+//        viewCharacter.setFitHeight(30);
+//        viewCharacter.setFitWidth(30);
+//        viewCharacter.setSmooth(true);
+//        viewCharacter.setPreserveRatio(true);
+//        final int personStartCoordinate = 350;
+//        viewCharacter.setX(personStartCoordinate);
+//        viewCharacter.setY(personStartCoordinate);
+    }
+    public static Player getInstance() {
+        if (singletonPlayer == null) {
+            singletonPlayer = new Player(IMAGE_CHARACTER);
+        }
+        return singletonPlayer;
+    }
+    public ImageView getViewCharacter() {
+        return viewCharacter;
+    }
 
-        final int personStartCoordinate = 350;
-        viewCharacter.setX(personStartCoordinate);
-        viewCharacter.setY(personStartCoordinate);
+    public static void characterXCoordinate(final double movement){
+        if (movement < appWidth - 30 && movement > 0) {
+            viewCharacter.setX(movement);
+        }
+    }
+    public static void characterYCoordinate(final double movement){
+        if (movement < appHeight - 30 && movement > 0) {
+            viewCharacter.setY(movement);
+        }
     }
 
     //    /**
@@ -39,16 +62,13 @@ public class Player {
 //     * @param event invoked this method
 //     */
     public void processKeyPress(final KeyEvent event) {
-
         switch (event.getCode()) {
-            case UP -> viewCharacter.setY(viewCharacter.getY() - MOVEMENT_SIZE);
-            case DOWN -> viewCharacter.setY(viewCharacter.getY() + MOVEMENT_SIZE);
-            case RIGHT -> viewCharacter.setX(viewCharacter.getX() + MOVEMENT_SIZE);
-            case LEFT -> viewCharacter.setX(viewCharacter.getX() - MOVEMENT_SIZE);
+            case UP -> Player.characterYCoordinate(Player.viewCharacter.getY() - MOVEMENT_SIZE);
+            case DOWN -> Player.characterYCoordinate(Player.viewCharacter.getY() + MOVEMENT_SIZE);
+            case RIGHT -> Player.characterXCoordinate(Player.viewCharacter.getX() + MOVEMENT_SIZE);
+            case LEFT -> Player.characterXCoordinate(Player.viewCharacter.getX() - MOVEMENT_SIZE);
             default -> {
             } // Does nothing if it's not an arrow key
         }
     }
 }
-
-
