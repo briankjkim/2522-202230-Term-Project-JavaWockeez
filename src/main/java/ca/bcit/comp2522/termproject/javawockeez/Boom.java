67852e691bda5f041e5b8cee4f2526f785091b70
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-12-02 13:19:48 -0800

    Begin building entity class and begin working on boom class

--- src/main/java/ca/bcit/comp2522/termproject/javawockeez/Boom.java
+++ src/main/java/ca/bcit/comp2522/termproject/javawockeez/Boom.java
@@ -2,7 +2,15 @@ package ca.bcit.comp2522.termproject.javawockeez;
 
 import javafx.scene.shape.Rectangle;
 
+import java.util.Random;
+
 public class Boom extends Rectangle implements Runnable {
+    private static final Random GENERATOR = new Random();
+    private static final int MAX_X = 510;
+    private static final int MAX_Y = 700;
+
+    private int dy;
+    private int dx;
     @Override
     public void run() {
 
---
+++ src/main/java/ca/bcit/comp2522/termproject/javawockeez/Entity.java
@@ -0,0 +1,48 @@
+package ca.bcit.comp2522.termproject.javawockeez;
+
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
+
+public abstract class Entity extends Group {
+    public static final int IMAGE_SIZE = 50;
+    protected Image entityImage;
+    protected ImageView entityImgView;
+    int size;
+    protected boolean isVisible = false;
+
+    public Entity() {
+
+    }
+
+    public Entity(final String entityImage) {
+        this(0, 0, entityImage);
+    }
+
+    public Entity(final int posX, final int posY, final String entityImage) {
+        this(posX, posY, IMAGE_SIZE, entityImage);
+    }
+
+    public Entity(final int posX, final int posY, final int imgSize, final String imgName) {
+        this.size = imgSize;
+        entityImage = new Image(Objects.requireNonNull(WallaceGameTest.class.getResource(imgName)).toExternalForm());
+        entityImgView = new ImageView(entityImage);
+        entityImgView.setFitWidth(this.size);
+        entityImgView.setPreserveRatio(true);
+        entityImgView.setSmooth(true);
+        entityImgView.setCache(true);
+        this.getChildren().add(entityImgView);
+        this.setTranslateX(posX);
+        this.setTranslateY(posY);
+    }
+
+    public int getCenterX() {
+        return (int) (this.getTranslateX() + (size / 2));
+    }
+
+    public int getCenterY() {
+        return (int) (this.getTranslateY() + (size / 2));
+    }
+}
