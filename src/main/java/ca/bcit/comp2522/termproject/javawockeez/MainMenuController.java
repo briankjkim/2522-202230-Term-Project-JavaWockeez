77a9e1a5520ebc8a07c4175b93a2abf48512f15f
WallaceTrinh <WTrinh1@my.bcit.ca>
2022-12-02 14:36:19 -0800

    Working on Main Menu files.

---
+++ src/main/java/ca/bcit/comp2522/termproject/javawockeez/MainMenuController.java
@@ -0,0 +1,27 @@
+//package ca.bcit.comp2522.termproject.javawockeez;
+//
+//import javafx.fxml.FXML;
+//import javafx.scene.control.Button;
+//import javafx.stage.Stage;
+//
+//import java.net.URISyntaxException;
+//public class MainMenuController {
+//
+//    @FXML public Button startButton;
+//    @FXML public Button exitButton;
+//
+//    @FXML
+//    protected void onStartGameButtonClick() throws URISyntaxException {
+//        GameDriver gameDriver = new GameDriver();
+//        Stage primaryScreen = new Stage();
+//        primaryScreen.setTitle("Escape Subject 2522");
+////        primaryScreen.setScene(gameDriver.getScene());
+//        primaryScreen.show();
+//    }
+//
+//    @FXML
+//    protected void onQuitGameButtonClick() {
+//        Stage stage = (Stage) exitButton.getScene().getWindow();
+//        stage.close();
+//    }
+//}
