5e4103ecc70fbcfbe076cbcada5ebb8bc7dfc06c
WallaceTrinh <WTrinh1@my.bcit.ca>
2022-12-02 22:15:16 -0800

    Added a main menu to the game in GameDriver.java file.

--- src/main/java/ca/bcit/comp2522/termproject/javawockeez/GameDriver.java
+++ src/main/java/ca/bcit/comp2522/termproject/javawockeez/GameDriver.java
@@ -1,16 +1,19 @@
 package ca.bcit.comp2522.termproject.javawockeez;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.stage.Stage;
 
 import java.nio.file.Paths;
-
 
 /**
  * Main Game Driver
@@ -18,14 +21,11 @@ import java.nio.file.Paths;
  * @version Dec 2022
  */
 public class GameDriver extends Application {
-
     public static final int appWidth = 510;
     public static final int appHeight = 700;
     private Player player;
     private ImageView viewBackground;
     private ImageView viewPersonTwo;
-
-
     /**
      * Audio Player for when the Game loads in.
      */
@@ -37,18 +37,67 @@ public class GameDriver extends Application {
         audioPlayer.setVolume(0.25);
         audioPlayer.play();
     }
+
+
+    /**
+     * Launches the JavaFX application.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Escape Subject 2522");
+        stage.setWidth(510);
+        stage.setHeight(725);
+        stage.setScene(mainMenu(stage));
+        stage.show();
+    }
+
+    public Scene mainMenu(Stage stage){// Creating a button in the first scene to be placed at the center
+        Button startButton = new Button();
+        startButton.setPrefWidth(100);
+        startButton.setPrefHeight(20);
+
+        startButton.setText("Start");
+
+        HBox hbox1 = new HBox(startButton);
+        hbox1.setSpacing(10);
+
+        hbox1.setAlignment(Pos.CENTER);
+
+        BorderPane mainMenuPane = new BorderPane();// It is used in order to set it at the center
+        mainMenuPane.setCenter(hbox1);
+
+        startButton.setOnAction(actionEvent -> {
+            stage.close();
+            try {
+                stage.setScene(inGame(stage));
+            } catch (Exception e) {
+                //ignore
+            }
+            stage.show();
+        });
+
+        return new Scene(mainMenuPane);
+    }
     /**
      * Displays an image that can be moved using the arrow keys.
      *
      * @param primaryStage a Stage
      */
-    public void start(final Stage primaryStage) {
+    public Scene inGame(final Stage stage) throws Exception {
         inGameMusic();
         Image person_two = new Image("ExplosionSpriteFPS7.gif", true);
         Image background = new Image("BoardTemplate.png", true);
 
         viewBackground = new ImageView(background);
         viewPersonTwo = new ImageView(person_two);
+
+        BorderPane gamePane = new BorderPane();
 
         final int explosionStartCoordinateX = 170;
         final int explosionStartCoordinateY = 261;
@@ -67,21 +116,12 @@ public class GameDriver extends Application {
 
         Scene scene = new Scene(root, appWidth, appHeight);
 
-        // Register the key listener here
         scene.setOnKeyPressed(player::processKeyPress);
-//        scene.setOnKeyPressed(player::processKeyRelease);
-        primaryStage.setResizable(false);
-        primaryStage.setTitle("Escape Subject 2522");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setResizable(false);
+        stage.setTitle("Escape Subject 2522");
+        stage.setScene(scene);
+        stage.show();
+        return new Scene(scene.getRoot());
     }
 
-    /**
-     * Launches the JavaFX application.
-     *
-     * @param args command line arguments
-     */
-    public static void main(final String[] args) {
-        launch(args);
-    }
 }
--- src/main/java/ca/bcit/comp2522/termproject/javawockeez/MainMenuDriver.java
+++ src/main/java/ca/bcit/comp2522/termproject/javawockeez/MainMenuDriver.java
@@ -1,41 +1,40 @@
-//package ca.bcit.comp2522.termproject.javawockeez;
-//
-//import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
-//import java.io.IOException;
-//
-//public class MainMenuDriver extends Application {
-//    private final String mainMenu  = "Escape Subject 2522";
-//    private final int appHeight = 700;
-//    private final int appWidth  = 510;
-//
-//    /**
-//     * Creates a main menu screen.
-//     * @param stage a scene
-//     * @throws IOException if FXML file cannot be found.
-//     */
-//    @Override
-//    public void start(final Stage stage) throws IOException {
-//
-//        try {
-//            FXMLLoader fxmlLoader = new FXMLLoader(MainMenuDriver.class.getResource("/MainMenu.fxml"));
-//            Scene scene = new Scene(fxmlLoader.load(), appWidth, appHeight);
-//            stage.setTitle(mainMenu);
-//            stage.setScene(scene);
-//            stage.show();
-//
-//        } catch (IOException ioe) {
-//            throw new IOException("Cannot find file");
-//        }
-//    }
-//
-//    /**
-//     * Drives the game.
-//     * @param args arguments.
-//     */
-//    public static void main(final String[] args) {
-//        launch();
-//    }
-//}
+package ca.bcit.comp2522.termproject.javawockeez;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+import static ca.bcit.comp2522.termproject.javawockeez.GameDriver.appHeight;
+import static ca.bcit.comp2522.termproject.javawockeez.GameDriver.appWidth;
+public class MainMenuDriver extends Application {
+    /**
+     * Creates a main menu screen.
+     * @param stage a scene
+     * @throws IOException if FXML file cannot be found.
+     */
+    @Override
+    public void start(final Stage stage) throws IOException {
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainMenuDriver.class.getResource("/MainMenu.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), appWidth, appHeight);
+            String mainMenu = "Escape Subject 2522";
+            stage.setTitle(mainMenu);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException ioe) {
+            throw new IOException("Cannot find file");
+        }
+    }
+
+    /**
+     * Drives the game.
+     * @param args arguments.
+     */
+    public static void main(final String[] args) {
+        launch();
+    }
+}
